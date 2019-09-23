c3b9970ea44cc3b49bd87672e74e175d4fabbab5
Nipat <nipat.pueksasuwan@gmail.com>
2019-09-23 17:31:57 +0700

    update

--- src/app/forms/side-nav/side-nav.component.ts
+++ src/app/forms/side-nav/side-nav.component.ts
@@ -13,13 +13,8 @@ export class SideNavComponent implements OnInit {
   closeResult: string;
   selectedColor: string;
   constructor(
-<<<<<<< HEAD
-     private auth: AuthService,
-    private modalService: NgbModal
-=======
      public auth: AuthService,
     //  private auth: AuthService,
->>>>>>> f68954274e9c21a100db6253785e094ef0122f10
      ) { }
 
   ngOnInit() {
