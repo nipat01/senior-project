a80bd707d8617052a552e19027c0b46f79a74c6f
Nipat <nipat.pueksasuwan@gmail.com>
2019-10-04 17:33:31 +0700

    update overview

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -38,6 +38,9 @@ export class OverviewComponent implements OnInit, OnChanges {
   publicEndDate;
   checkStartDate;
   checkEndDate;
+  //รายชื่อพนักงาน เอาไว้เก็บ review
+  totaoReview = 0;
+  empList: any[];
   get color() {
     return AppComponent.COLOR ? AppComponent.COLOR : AppComponent.DEFAULTCOLOR;
   }
@@ -89,6 +92,7 @@ export class OverviewComponent implements OnInit, OnChanges {
   }
 
   searchJob(data1) {
+    this.totalReview = 0;
     //กำหนด true ไว้ ในกรณีที่ มีค่าวันที่ ต้นทาง กับปลายจะ จะเอาไปfilter อีกfunction
     this.checkEndDate = true;
     this.checkStartDate = true;
@@ -99,6 +103,14 @@ export class OverviewComponent implements OnInit, OnChanges {
     //   console.log(wikis)
     //   this.wikis = wikis;
     // });
+    this.wikis.length
+    console.log('this.wikis.length', this.wikis.length);
+    for (let index = 0; index < this.wikis.length; index++) {
+      console.log('length', this.wikis[index].value.firstname);
+    }
+
+    console.log('empList', this.empList);
+
 
 
     console.log(data1.value);
@@ -126,22 +138,59 @@ export class OverviewComponent implements OnInit, OnChanges {
     const endDate = new Date(data1.value.endworkDate.year, data1.value.endworkDate.month - 1, data1.value.endworkDate.day)
     this.publicStartDate = startDate;
     this.publicEndDate = endDate;
+    if (this.checkStartDate === true && this.checkEndDate === true) {
 
-    // this.db.list('job').snapshotChanges().map(actions => {
-    //   return actions.map(action => ({ key: action.key, value: action.payload.val() }));
-    // }).subscribe(job => {
-    //   console.log(job);
-    //   const startDate = new Date(data1.value.workDate.year, data1.value.workDate.month - 1, data1.value.workDate.day);
-    //   const endDate = new Date(data1.value.endworkDate.year, data1.value.endworkDate.month - 1, data1.value.endworkDate.day)
-    //   this.job = job.filter((data: any) => {
+      this.db.list('job').snapshotChanges().map(actions => {
+        return actions.map(action => ({ key: action.key, value: action.payload.val() }));
+      }).subscribe(job => {
+        console.log(job);
+        const startDate = new Date(data1.value.workDate.year, data1.value.workDate.month - 1, data1.value.workDate.day);
+        const endDate = new Date(data1.value.endworkDate.year, data1.value.endworkDate.month - 1, data1.value.endworkDate.day)
+        this.job = job.filter((data: any) => {
 
-    //     const jobDate = new Date(data.value.workDate.year, data.value.workDate.month - 1, data.value.workDate.day);
-    //     return jobDate.getTime() >= startDate.getTime() && jobDate.getTime() <= endDate.getTime();
-    //   }
+          const jobDate = new Date(data.value.workDate.year, data.value.workDate.month - 1, data.value.workDate.day);
+          return jobDate.getTime() >= startDate.getTime() && jobDate.getTime() <= endDate.getTime();
+        })
+        console.log('lengthJob', this.job.length);
+        for (let empIndex = 0; empIndex < this.wikis.length; empIndex++) {
+          // console.log('lengthJob',this.job.length);
+          let wikis = this.wikis[empIndex].value
+          const tmp = []
+          // console.log('job', job);
 
 
-    //   )
-    // });
+          let averageRating = 0;
+          let sum = 0;
+          let count = 0;
+          for (let index = 0; index < this.job.length; index++) {
+            if (this.job[index].value.rateReview) {
+              if (wikis.firstname === this.job[index].value.driver) {
+                console.log('wikis', this.wikis[empIndex].value.firstname,
+                  'driver', this.job[index].value.driver);
+                console.log('job', this.job[index].value);
+                sum = this.job[index].value.rateReview + sum;
+                count++;
+                console.log('sum', sum, 'count', count);
+
+              }
+            }
+
+          }
+
+          this.wikis[empIndex].value.rateReview = sum / count;
+          console.log('this.wikis[empIndex].value.rateReview', this.wikis[empIndex].value.rateReview);
+          if (this.totalReview = 0) {
+            this.totalReview = this.totalReview + this.wikis[empIndex].value.rateReview;
+          }
+          this.totalReview = (this.totalReview + this.wikis[empIndex].value.rateReview) / 2
+          // objectReturn.rateReview = averageRating
+
+          // console.log('review', this.job[index].value.rateReview);
+          // this.empList = [...tmp];
+        }
+        // console.log('empList', this.empList);
+      });
+    }
 
   }
 
