b9a685d2593870a54f8bc6dfdeded653ca10017d
nipat01 <52153124+nipat01@users.noreply.github.com>
2021-01-31 21:05:39 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
     firebaseConfig: {
-    apiKey: 'AIzaSyBbdxHq0NSUmlfE87RVqbtZ-Kii6JcTj2Q',
-    authDomain: 'jistigdatabase.firebaseapp.com',
-    databaseURL: 'https://jistigdatabase.firebaseio.com',
-    projectId: 'jistigdatabase',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
     storageBucket: 'jistigdatabase.appspot.com',
-    messagingSenderId: '17341884881',
-    appId: '1:17341884881:web:4d312c299f80ffe8'
+    messagingSenderId: '',
+    appId: ''
   },
-  googleMapsKey: 'AIzaSyD7vq-yq_csTGNqXl-XtJ8OJKXsl4Akq9c'
+  googleMapsKey: ''
 };
